487e8479c36e2be09a1fbb4dee90b3e2833ba6e8
cyrockss <cyrockss@163.com>
2019-09-17 15:03:51 +0800

    caoyu

---
+++ SocketLib/MySocket.py
@@ -0,0 +1,7 @@
+import socket
+
+class MySocket():
+    def connetServer(self, server_ip, server_port):
+        # 建立连接:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((server_ip, int(server_port)))
---
+++ SocketLib/__init__.py
@@ -0,0 +1,7 @@
+#coding: utf-8
+
+from MySocket import MySocket
+_version_ = '1.0'
+
+class SocketLib(MySocket):
+    ROBOT_LIBRARY_SCOPE = 'GLOBAL'
